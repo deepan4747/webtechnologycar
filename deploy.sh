58666a053b4b6a5f4a5313c5e088a9828a7afffd
deepan4747 <127740629+deepan4747@users.noreply.github.com>
2025-04-04 12:56:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,6 @@ echo "Deepan11032004" | docker login -u "deepan47" --password-stdin
 # Tag and push the image
 docker tag finaltask deepan47/finaltask
 docker push deepan47/finaltask
-docker pull deepan47/finaltask
+
 docker run -d -p 8050:80 --name=my-service-repo deepan47/finaltask
+docker pull deepan47/finaltask
